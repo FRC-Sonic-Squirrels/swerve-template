82654a6bbbebb92f721b8668bad27d3b7ce3a85b
LuminousLlama <keyushattarde@gmail.com>
2022-02-25 16:43:06 -0800

    set robo pose at robotInit

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -30,6 +30,8 @@ public class Robot extends TimedRobot {
     // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
     // autonomous chooser on the dashboard.
     m_robotContainer = new RobotContainer(this);
+    m_robotContainer.drivetrainSubsystem.setGyroscopeHeadingDegrees(0);
+    m_robotContainer.drivetrainSubsystem.setPose(Constants.ROBOT_1M_LEFT_OF_HUB, new Rotation2d());
   }
 
   /**
